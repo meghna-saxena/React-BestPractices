f08be7fd20967a3ad835445430c4e6707a24e811
meghna-saxena <meghna.s1906@gmail.com>
2017-10-21 21:38:07 +0530

    changing data with the input field

--- countdownTimer/src/app.jsx
+++ countdownTimer/src/app.jsx
@@ -5,14 +5,15 @@ class App extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        deadline: 'December 25, 2017'
+        deadline: 'December 25, 2017',
+        newDeadline: ''
       }
     }
 
     changeDeadline() {
         this.setState(
-            {deadline: 'November 25, 2017'}
-        )
+            {deadline: this.state.newDeadline}
+        );
     }
 
     render() {
@@ -28,7 +29,10 @@ class App extends Component {
                     <div className="Clock-seconds">20 seconds</div>
                 </div>
                 <div>
-                    <input placeholder="new date" />
+                    <input 
+                        placeholder="new date"
+                        onChange={event => this.setState({newDeadline: event.target.value})} 
+                     />
                     <button onClick={() => this.changeDeadline()}>Submit</button>
                 </div>
             </div>
