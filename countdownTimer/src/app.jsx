0763cdc95f7ffbfd7e6a16060bafb4291fc6289f
meghna-saxena <meghna.s1906@gmail.com>
2017-10-21 20:09:49 +0530

    updating state

--- countdownTimer/src/app.jsx
+++ countdownTimer/src/app.jsx
@@ -9,6 +9,12 @@ class App extends Component {
       }
     }
 
+    changeDeadline() {
+        this.setState(
+            {deadline: 'November 25, 2017'}
+        )
+    }
+
     render() {
         return (
           <div className="App">
@@ -23,7 +29,7 @@ class App extends Component {
                 </div>
                 <div>
                     <input placeholder="new date" />
-                    <button>Submit</button>
+                    <button onClick={() => this.changeDeadline()}>Submit</button>
                 </div>
             </div>
         );
