2b9b5dc7abb35fee45584e7cfb00653ed73da74c
meghna-saxena <meghna.s1906@gmail.com>
2017-10-21 19:48:29 +0530

    initialized state

--- countdownTimer/src/app.jsx
+++ countdownTimer/src/app.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './app.css';
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        deadline: 'December 25, 2017'
+      }
+    }
+
     render() {
         return (
-            <div className="App">
-                <div className="App-title">Countdown to December 25, 2017</div>
+          <div className="App">
+            <div className="App-title">
+              Countdown to {this.state.deadline}
+            </div>
                 <div>
                     <div className="Clock-days">14 days</div>
                     <div className="Clock-hours">30 hours</div>
