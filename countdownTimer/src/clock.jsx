3a84eb27e0c7499d20011c249931fc91f4271028
meghna-saxena <meghna.s1906@gmail.com>
2017-10-22 16:57:16 +0530

    distinguishing the time values by Math.floor()

--- countdownTimer/src/clock.jsx
+++ countdownTimer/src/clock.jsx
@@ -15,7 +15,18 @@ class Clock extends Component {
 
     getTimeUntil(deadline) {
         const time = Date.parse(deadline) - Date.parse(new Date());
-    }
+        console.log('time', time);
+
+        const seconds = Math.floor((time/1000) % 60);
+        const minutes = Math.floor((time/1000/60) % 60);
+        const hours = Math.floor(time/(1000*60*60) % 24);
+        const days = Math.floor(time/(1000*60*60*24));
+
+        console.log('seconds', seconds,'minutes', minutes,'hours', hours, 'days', days);
+    
+        //update the state
+        this.setState({days: days});
+      }
 
     render() {
         this.getTimeUntil(this.props.deadline);
