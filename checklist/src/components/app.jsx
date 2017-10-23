bfd360547514f59e4524bec459b43699c126b78d
meghna-saxena <meghna.s1906@gmail.com>
2017-10-23 16:09:53 +0530

    app setup

---
+++ checklist/src/components/app.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="title">
+                    Checklist App
+                </div>
+                <div className="form-inline">
+                    <div className="form-group">
+                        <input className="form-control"
+                               placeholder="Write the upcoming tasks.."
+                        />
+                    </div>
+                    <button type="button" className="btn btn-success">
+                        Add Reminder
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
