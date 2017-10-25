00eeecf0b98e65844a9b2f535f319d0323e9cd6e
Meghna Srivastava <meghna.s1906@gmail.com>
2017-10-25 16:47:30 +0530

    rendering list of reminders

--- checklist/src/components/app.jsx
+++ checklist/src/components/app.jsx
@@ -16,10 +16,27 @@ class App extends Component {
         // console.log('this', this);
         this.props.addReminder(this.state.text)
     }
-    
+
+    renderReminders() {
+        //return <ul> of reminders submitted by user
+        const {reminders} = this.props;
+        console.log('reminders', reminders);
+
+        return (
+            <ul className="list-group col-sm-4">
+                {reminders.map(reminder => {
+                    return (
+                        <li key={reminder.id} className="list-group-item">
+                            <div>{reminder.text}</div>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
 
     render() {
-        console.log('this.props', this.props);
         return (
             <div className="App">
                 <div className="title">
@@ -38,6 +55,7 @@ class App extends Component {
                         Add Reminder
                     </button>
                 </div>
+                { this.renderReminders() }
             </div>
         );
     }
