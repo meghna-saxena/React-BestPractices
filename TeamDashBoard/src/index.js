6826eacbb597b9ca4452c7ac3e680b9490ed1363
Meghna Srivastava <meghna.s1906@gmail.com>
2017-10-31 14:01:20 +0530

    initial commit

---
+++ TeamDashBoard/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import SignIn from './components/signin';
+import SignUp from './components/signup';
+
+ReactDOM.render (
+    <App />, document.querySelector('#root')
+)
