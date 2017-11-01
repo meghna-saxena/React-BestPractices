1696f4b53c53cdb58e799ee0b1f0cc017e11367a
Meghna Srivastava <meghna.s1906@gmail.com>
2017-11-01 20:48:16 +0530

    made a signup form

--- TeamDashBoard/src/components/signup.jsx
+++ TeamDashBoard/src/components/signup.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 class SignUp extends Component {
     render() {
         return (
-            <div>Sign up</div>
+            <div className="form-inline">
+                <h2>Sign Up</h2>
+                <div className="form-group">
+                    <input className="form-control"
+                            type="text"
+                            placeholder="email"
+                    />
+                    <input className="form-group"
+                            type="password"
+                            placeholder="password"
+                    />
+                    <button className="btn btn-primary" type="button">Sign Up</button>
+                </div>
+            </div>
         );
     }
 }
