4344002b6358998547981c7d2f3a90b2b1cab1ad
Meghna Srivastava <meghna.s1906@gmail.com>
2017-11-01 20:08:18 +0530

    Added react router

--- TeamDashBoard/src/index.js
+++ TeamDashBoard/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
 import App from './components/app';
 import SignIn from './components/signin';
 import SignUp from './components/signup';
 
 ReactDOM.render (
-    <App />, document.querySelector('#root')
+    <Router path="/" history={browserHistory}>
+        <Route path="/app" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+    </Router>
+    , document.querySelector('#root')
 )
