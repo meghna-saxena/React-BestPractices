529bcae7379b82dd81c561362169e28a76d5fa2d
Meghna Srivastava <meghna.s1906@gmail.com>
2017-11-01 20:31:06 +0530

    Added firebase to the application

---
+++ TeamDashBoard/src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+//Add Firebase to web app by pasting this snippet
+//generated from firebase.google.com
+const config = {
+    apiKey: "AIzaSyA_Cv7fQTvQ79FlCUUmaL0aDQyO5PBne8E",
+    authDomain: "teamdashboard-5d670.firebaseapp.com",
+    databaseURL: "https://teamdashboard-5d670.firebaseio.com",
+    projectId: "teamdashboard-5d670",
+    storageBucket: "",
+    messagingSenderId: "808520737041"
+  };
+
+  export const firebaseApp = firebase.initializeApp(config);
--- TeamDashBoard/src/index.js
+++ TeamDashBoard/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-
+import { firebaseApp } from './firebase';
 import App from './components/app';
 import SignIn from './components/signin';
 import SignUp from './components/signup';
+
+firebaseApp.auth().onAuthStateChanged(user => {
+    if(user) {
+        console.log('User has signed in or signed up', user);
+    } else {
+        console.log('User has signed out or still needs to signin', user);
+    }
+})
 
 ReactDOM.render (
     <Router path="/" history={browserHistory}>
