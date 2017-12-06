aa94ed716168294f05301584f7fc245f6cb382e2
Meghna Srivastava <meghna.s1906@gmail.com>
2017-12-06 17:35:15 +0100

    rendering list of goals on console

--- TeamDashBoard/src/components/goal_list.jsx
+++ TeamDashBoard/src/components/goal_list.jsx
@@ -4,10 +4,16 @@ import { goalRef } from '../firebase';
 class GoalList extends Component {
     componentDidMount() {
         goalRef.on('value', snap => {
+            let goals = []
             snap.forEach(goal => {
-                let goalObject = goal.val();
-                console.log('goalObject', goalObject);
+                //let goalObject = goal.val();
+                //since goalObject has email & title
+                const { email, title } = goal.val();
+                // console.log('goalObject', goalObject);
+                // goals.push(goalObject); => rewrite
+                goals.push({ email, title });
             })
+            console.log('goals', goals);
         })
     }
 
