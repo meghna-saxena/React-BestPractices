4b5f11e5a776080db161f947176d55719d291766
Meghna Srivastava <meghna.s1906@gmail.com>
2017-12-18 15:59:27 +0100

    project init

---
+++ TextGenerator/src/components/app.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    // componentDidMount() {
+    //     console.log('React running ..');
+    // }
+
+    render() {
+        return (
+            <div>Dummy Text Generator</div>
+        );
+    }
+}
+
+export default App;
