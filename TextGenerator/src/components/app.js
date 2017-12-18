155b651e1e011dab53fce21833c70695b79401cc
Meghna Srivastava <meghna.s1906@gmail.com>
2017-12-18 16:51:18 +0100

    fetching data from api to get dummy text

--- TextGenerator/src/components/app.js
+++ TextGenerator/src/components/app.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
-    // componentDidMount() {
-    //     console.log('React running ..');
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            paras: 4,
+            html : false,
+            text: ''
+        }
+    }
+
+    componentWillMount() {
+        this.getText();
+    }
+
+    getText() { 
+        axios.get('http://hipsterjesus.com/api/?paras='+this.state.paras+'&html='+this.state.html)
+        .then((response) => {
+            this.setState({text: response.data.text}, function() {
+                console.log(this.state);
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
 
     render() {
         return (
