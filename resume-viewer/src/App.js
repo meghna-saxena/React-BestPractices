efd21d0dde11cf7aa37219ac1ff714bb56ae1ac3
Meghna Srivastava <meghna.s1906@gmail.com>
2017-12-26 16:09:35 +0100

    fetching data from json file

--- resume-viewer/src/App.js
+++ resume-viewer/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import Header from './components/header';
 import About from './components/about';
 import Resume from './components/resume';
@@ -9,7 +10,32 @@ import Contact from './components/contact';
 import Footer from './components/footer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // foo: 'bar',
+      resumeData: {}
+    }
+  }
+
+  getResumeData() {
+    axios.get('http://localhost:3000/resumeData.json')
+      .then((response) => {
+        this.setState({resumeData: response.data}, function() {
+          console.log(this.state);
+        });
+    })
+      .catch((err) => {
+        console.log(err);
+    });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
   render() {
+    console.log(this.state.resumeData);
     return (
       <div className="App">
         <Header />
--- resume-viewer/src/components/header.js
+++ resume-viewer/src/components/header.js
@@ -4,21 +4,17 @@ class Header extends Component {
   render() {
     return (
       <header id="home">
-
-      <nav id="nav-wrap">
-
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a classNameName="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	        <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <ul id="nav" className="nav">
+              <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                <li><a className="smoothscroll" href="#about">About</a></li>
+	              <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+                <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            </ul>
       </nav>
 
       <div className="row banner">
