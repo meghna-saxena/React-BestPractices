2cb6c269748b15f7fa751d0089704ea23349c0aa
Meghna <meggie@Rajs-MacBook-Pro.local>
2018-03-05 15:17:19 +0100

    practicing react componentization

---
+++ dummy-exercise/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="person">
+      <h1>Meggie</h1>
+      <p> Your age: 28</p>
+      </div>
+    );
+  }
+}
+
+export default App;
