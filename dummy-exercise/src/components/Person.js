388ae8bca4f2073448b70baddc31f0b3269fe460
Meghna Srivastava <meghna.s1906@gmail.com>
2018-03-05 16:54:19 +0100

    rendered data by using props

--- dummy-exercise/src/components/Person.js
+++ dummy-exercise/src/components/Person.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../index.css';
 
-class App extends Component {
+class Person extends Component {
   render() {
     return (
       <div className="person">
@@ -12,4 +12,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Person;
--- dummy-exercise/src/index.js
+++ dummy-exercise/src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import Person from './components/Person';
 
-ReactDOM.render(<App name="Meggie" age="28" />, document.getElementById('app'));
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Person name="Meggie" age="28" />
+                <Person name="Max" age="29" />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
